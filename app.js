79ad8499638882dbbd5f92f523b76d7668588a3e
jjade seravla <jadealvares@hotmail.co.uk>
2018-05-18 12:02:36 +0100

    change this.status to 200 as now running on a node server on index.html

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// const http = require('http');
-// const fs = require('fs');
-//   path = require('path'),
-//   filePath = path.join(__dirname, 'index.html');
-//
-// const hostname = '127.0.0.1';
-// const port = 3000;
-//
-// fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-//     if (!err) {
-//         console.log('received data: ' + data);
-//         response.writeHead(200, {'Content-Type': 'text/html'});
-//         response.write(data);
-//         response.end();
-//     } else {
-//         console.log(err);
-//     }
-// });
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
@@ -46,7 +28,7 @@ http.createServer(function (request, response) {
 
     fs.readFile(filePath, function(error, content) {
         if (error) {
-            if(error.code == 'ENOENT'){
+            if(error.code == 'ENOENT'){ //Error No Such File or Directory
                 fs.readFile('./404.html', function(error, content) {
                     response.writeHead(200, { 'Content-Type': contentType });
                     response.end(content, 'utf-8');
@@ -66,3 +48,24 @@ http.createServer(function (request, response) {
 
 }).listen(8125);
 console.log('Server running at http://127.0.0.1:8125/');
+
+
+
+// const http = require('http');
+// const fs = require('fs');
+//   path = require('path'),
+//   filePath = path.join(__dirname, 'index.html');
+//
+// const hostname = '127.0.0.1';
+// const port = 3000;
+//
+// fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+//     if (!err) {
+//         console.log('received data: ' + data);
+//         response.writeHead(200, {'Content-Type': 'text/html'});
+//         response.write(data);
+//         response.end();
+//     } else {
+//         console.log(err);
+//     }
+// });
